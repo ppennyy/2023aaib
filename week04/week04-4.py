f8f0d2421298fb2790de5dab03d0ffeaadc70222
ppenny <12750245@me.mcu.edu.tw>
2023-10-06 12:16:01 +0800

    修改目錄

---
+++ week04/week04-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        
+        ans=1          #一開始的答案是正的
+        for n in nums: #依序取n出來
+            ans *=n    #答案 乘n
+
+        if ans>0: return 1
+        if ans<0: return -1
+        if ans==0: return 0
