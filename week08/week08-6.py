bb033cb4b882aa5b536f21ea0c4a1e357b74b6a9
ppennyy <12750245@me.mcu.edu.tw>
2023-11-03 11:59:17 +0800

    week08

---
+++ week08/week08-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        # 現在還沒寫完, 先把week08-3 的兩兩比較, 寫出來
+        N = len(arr) # 小心, 題目不是a是arr
+
+        #for k in range(N):
+        #   for i in range(1,N):
+        #       if arr[i] < arr[i-1]:
+        #           arr[i], arr[i-1] = arr[i-1], arr[i]
+        arr.sort()
+        
+        for i in range(1, N):
+            if arr[i] - arr[i-1] != arr[1] - arr[0]:
+                return False
+        return True
